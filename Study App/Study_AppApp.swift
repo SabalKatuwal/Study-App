b28c47427dca9bf092e67af9bb1ef1fe8fb77d87
SabalKatuwal <sabal.katuwal1@gmail.com>
2024-03-08 13:45:02 +0545

    Initial Commit

---
+++ Study App/Study_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Study_AppApp.swift
+//  Study App
+//
+//  Created by Sabal on 3/8/24.
+//
+
+import SwiftUI
+
+@main
+struct Study_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
