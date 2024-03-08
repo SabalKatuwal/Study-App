508393bbfe37b771b007fa176dde6e7b50f7ef17
SabalKatuwal <sabal.katuwal1@gmail.com>
2024-03-08 16:07:50 +0545

    TopView

---
+++ Study App/Views/TopView.swift
@@ -0,0 +1,18 @@
+//
+//  TopView.swift
+//  Study App
+//
+//  Created by Sabal on 3/8/24.
+//
+
+import SwiftUI
+
+struct TopView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TopView()
+}
