d9797e7db16d3bff57540f7f43c533242edc57a9
SabalKatuwal <sabal.katuwal1@gmail.com>
2024-03-08 16:08:01 +0545

    TopView

--- Study App/Views/ContentView.swift
+++ Study App/Views/ContentView.swift
@@ -14,55 +14,7 @@ struct ContentView: View {
     @State var currentSlider: Int = 0
     
     var body: some View {
-        ScrollView {
-            HStack {
-                //MARK: DRAWER MENU
-                Button {
-                    
-                } label: {
-                    Image(systemName: "lines.measurement.vertical")
-                        .renderingMode(.template)
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 25, height: 25)
-                } // END DRAWER MENU
-                
-                Spacer()
-                
-                //MARK: NOTIFY
-                Button {
-                    
-                } label: {
-                    Image(systemName: "bell")
-                        .font(.title2)
-                }
-                // END NOTIFY ICON
-                
-                //MARK: SEARCH ICON
-                Button {
-                    
-                } label: {
-                    Image(systemName: "magnifyingglass")
-                        .font(.title2)
-                } // END SEARCH ICON
-                
-            }
-            .foregroundColor(.black)
-            .overlay(
-                Image("Logo")
-                    .resizable()
-                    .frame(width: 124, height: 54)
-                    .offset(y: 4)
-                
-            )
-            // END APP BAR
-        }
-        .padding()
-        //MARK - APP BAR
-        
-        
-        
-        
+        TopView()
     }
 }
 
--- Study App/Views/TopView.swift
+++ Study App/Views/TopView.swift
@@ -8,8 +8,69 @@
 import SwiftUI
 
 struct TopView: View {
+    @EnvironmentObject var baseData: BaseViewModel
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading) {
+            HStack {
+    //                //MARK: DRAWER MENU
+    //                Button {
+    //                    
+    //                } label: {
+    //                    Image(systemName: "lines.measurement.vertical")
+    //                        .renderingMode(.template)
+    //                        .resizable()
+    //                        .aspectRatio(contentMode: .fit)
+    //                        .frame(width: 25, height: 25)
+    //                } // END DRAWER MENU
+                    
+                    
+    //                //MARK: NOTIFY
+    //                Button {
+    //
+    //                } label: {
+    //                    Image(systemName: "bell")
+    //                        .font(.title2)
+    //                }
+    //                // END NOTIFY ICON
+                    
+                    Spacer()
+
+                    
+                    //MARK: APP LOGO
+                    Image("Logo")
+                        .resizable()
+                        .frame(width: 124, height: 54)
+                        .offset(x: -25)
+                    // END APP LOGO
+                    
+                    //MARK: SEARCH ICON
+                    Button {
+                        
+                    } label: {
+                        Image(systemName: "magnifyingglass")
+                            .font(.title2)
+                    } // END SEARCH ICON
+                }
+                .foregroundColor(.black)
+            
+            Text("Hello")
+                .font(.title)
+                .fontWeight(.light)
+                .fontDesign(.monospaced)
+            Text("Sabal")
+                .font(.largeTitle)
+                .fontDesign(.monospaced)
+//                .fontWeight(.medium)
+        }
+        .padding()
+            // END APP BAR
+        
+        
+        
+        
+        
     }
 }
 
